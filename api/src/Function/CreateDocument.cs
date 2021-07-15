a774031c786f100d1f99bcf631ab744edab58b68
Yvan Razafindramanana <mail@ludeo.net>
2021-07-15 07:58:20 +0200

    Send editor content to process preview

--- api/src/Function/CreateDocument.cs
+++ api/src/Function/CreateDocument.cs
@@ -6,31 +6,87 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using System.IO;
 using System;
+using System.Text;
 
 namespace Ludeo.Antauvido.Api
 {
 	public static class CreateDocument
 	{
 		[FunctionName("CreateDocument")]
-		public static async Task<IActionResult> RunAsync(
+		public static async Task RunAsync(
 			[HttpTrigger(AuthorizationLevel.Function, "post", Route = "document")]
 			HttpRequest request,
-			IBinder binder,
+			IBinder binder, // use IBinder instead of blob output binding because path is dynamic
 			ILogger logger)
+		{
+			var document = await ReadRequestAsync(request);
+
+			var (documentId, blobPath) = await SaveDocumentAsync(document, binder);
+
+			logger.LogInformation("Created document {BlobPath}", blobPath);
+
+			if (request.HasFormContentType)
+			{
+				RedirectToPreview(request, documentId);
+			}
+			else
+			{
+				// no input provided, just create the document and return its URI
+				await OutputDocumentLocationAsync(request, documentId);
+			}
+		}
+
+		private static async Task<string> ReadRequestAsync(HttpRequest request)
+		{
+			if (request.HasFormContentType)
+			{
+				// TODO sanitize user input
+
+				var form = await request.ReadFormAsync();
+				var document = form["editor"];
+
+				if (document.Count > 0)
+				{
+					return document[0];
+				}
+			}
+
+			return "";
+		}
+
+		private static async Task<(Guid documentId, string blobPath)> SaveDocumentAsync(
+			string document,
+			IBinder binder)
 		{
 			var documentId = Guid.NewGuid();
 
-			// use IBinder from WebJobs because path of blob is dynamic
 			var blobPath = $"documents/{documentId}";
 			using var outputStream = await binder.BindAsync<Stream>(
 				new BlobAttribute(blobPath, FileAccess.Write));
 
-			await request.Body.CopyToAsync(outputStream);
+			using var documentStream = new StreamWriter(outputStream);
+			await documentStream.WriteAsync(document);
 
-			logger.LogInformation("Created document {BlobPath}", blobPath);
+			return (documentId, blobPath);
+		}
 
-			request.HttpContext.Response.Headers.Add("Location", $"/api/preview/{documentId}");
-			return new StatusCodeResult(StatusCodes.Status201Created);
+		private static void RedirectToPreview(HttpRequest request, Guid documentId)
+		{
+			var previewUri = $"/api/preview/{documentId}";
+			request.HttpContext.Response.Headers.Add("Location", previewUri);
+
+			request.HttpContext.Response.StatusCode = StatusCodes.Status303SeeOther;
+		}
+
+		private static async Task OutputDocumentLocationAsync(HttpRequest request, Guid documentId)
+		{
+			var documentUri = $"/api/document/{documentId}";
+			request.HttpContext.Response.Headers.Add("Content-Location", documentUri);
+
+			request.HttpContext.Response.StatusCode = StatusCodes.Status201Created;
+
+			// if Content-Location header cannot be read (e.g. because of CORS) document URI is made available in response body
+			await request.HttpContext.Response.Body.WriteAsync(Encoding.UTF8.GetBytes(documentUri));
 		}
 	}
 }
--- app/src/main.js
+++ app/src/main.js
@@ -1,5 +1,27 @@
 const config = {
-	apiUrl: import.meta.env.VITE_API_URL
+	apiUrl: import.meta.env.VITE_API_URL,
+	newDocumentUrl: import.meta.env.VITE_API_NEW_DOCUMENT_URL,
+};
+
+window.addEventListener("DOMContentLoaded", async () => {
+	// Initialise an empty document and update the form action to the new document URL.
+ 	// Default behaviour (e.g. when JS is disabled) is to create a new document on every submit.
+	// If initialisation successful, submit always updates the same document.
+	var newDocumentUri = await initialiseNewDocument();
+	updateFormUrl(newDocumentUri);
+});
+
+async function initialiseNewDocument() {
+	const newDocument = await fetch(config.newDocumentUrl, { method: "post" });
+
+	// cannot read Content-Location header (because of CORS) so read URI in response body
+	const newDocumentUri = await newDocument.text();
+	return newDocumentUri;
 }
 
-console.debug("Configuration:", config);
+function updateFormUrl(actionUri) {
+	var forms = document.getElementsByTagName("form");
+	for (let i = 0; i < forms.length; ++i) {
+		forms[i].action = config.apiUrl + actionUri;
+	}
+}
